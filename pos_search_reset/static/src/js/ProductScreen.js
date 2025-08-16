c41c847842d7fec06997a649cd8fbccd0ef3d42d
Muhammad Sharjeel <ms.servicesofficial@gmail.com>
2025-08-17 01:38:11 +0500

    remove unused import

--- pos_search_reset/static/src/js/ProductScreen.js
+++ pos_search_reset/static/src/js/ProductScreen.js
@@ -3,8 +3,6 @@
 import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
-import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
-import { ConfirmPopup } from "@point_of_sale/app/utils/confirm_popup/confirm_popup";
 
 patch(PosStore.prototype, {
     async addProductToCurrentOrder(product, options = {}) {
